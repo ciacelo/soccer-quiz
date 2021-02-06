67ab6bf151c3d75c21ca62f68f7b788bfb45068e
ciacelo <marcelo.martins@dcx.ufpb.br>
2021-02-05 22:00:47 -0300

    add formatação eslint e font global

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from "../db.json"
-
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -12,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
     /* New styles */
     display: flex;
     flex-direction: column;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Roboto', sans-serif;
     // Deixa branco no começo
     color: ${({ theme }) => theme.colors.contrastText};
   }
@@ -24,17 +25,26 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Jogo do Quiz | Futebol na Veia</title>
+        <meta name="description" content="Este Quiz vai lhe testar, quanto aos seus conhecimentos 'futebolisticos'." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,17 @@
-import styled from 'styled-components'
-import db from "../db.json"
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackGround'
-import QuizContainer from '../src/components/QuizContainer'
-import Widget from "../src/components/Widget"
-import Head from 'next/head'
-import QuizLogo from '../src/components/QuizLogo'
-
-
+import React from 'react';
+import db from '../db.json';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackGround';
+import QuizContainer from '../src/components/QuizContainer';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
 
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <Head>
-        <title>Jogo do Quiz | Futebol na Veia</title>
-        <meta name="description" content="Este Quiz vai lhe testar, quanto aos seus conhecimentos 'futebolisticos'."/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </Head>
       <QuizContainer>
-        <QuizLogo/>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>The legend of Messi</h1>
@@ -27,16 +19,16 @@ export default function Home() {
           <Widget.Content>
             <h1>O que você conhece sobre futebol?</h1>
           </Widget.Content>
-          
+
         </Widget>
         <Widget>
           <Widget.Content>
             <h1>Quiz da Galera:</h1>
           </Widget.Content>
         </Widget>
-        <Footer/>
-        <GitHubCorner projectUrl={"https://github.com/ciacelo/soccer-quiz"}/>
+        <Footer />
+        <GitHubCorner projectUrl="https://github.com/ciacelo/soccer-quiz" />
       </QuizContainer>
     </QuizBackground>
-  )
+  );
 }
