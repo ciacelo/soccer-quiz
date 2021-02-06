227d14a005f8ded8d90edda79132af88cb881003
ciacelo <marcelo.martins@dcx.ufpb.br>
2021-02-06 12:11:21 -0300

    criando página quiz

---
+++ pages/quiz.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const QuizPage = () => {
+  const name = 'marcelo';
+  return (
+    <>
+      <div>
+        página quiz:
+        {' '}
+        {name}
+      </div>
+    </>
+  );
+};
+
+export default QuizPage;
