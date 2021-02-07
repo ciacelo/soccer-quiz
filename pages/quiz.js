748d40cfedf7055e8524826476b056489eb1d534
ciacelo <marcelo.martins@dcx.ufpb.br>
2021-02-07 19:19:04 -0300

    updates na página do quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackGround';
+import QuizContainer from '../src/components/QuizContainer';
+import GitHubCorner from '../src/components/GitHubCorner';
+// import Input from '../src/components/Input';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import QuestionWidget from '../src/components/QuizPage/QuestionWidget';
 
 const QuizPage = () => {
-  const name = 'marcelo';
+  const [x, setX] = useState('');
+  const { questions } = db;
+
   return (
-    <>
-      <div>
-        página quiz:
-        {' '}
-        {name}
-      </div>
-    </>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <QuestionWidget
+          questions={questions}
+        />
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da Galera:</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+        <GitHubCorner projectUrl="https://github.com/ciacelo/soccer-quiz" />
+      </QuizContainer>
+    </QuizBackground>
   );
 };
 
